fefa91386b6805874b229dced52ac819c40f11c4
李建祥[credential] <jianxianglisir@163.com>
2025-04-07 18:12:00 +0800

    refactor(api): 删除设备参数并简化命令行参数

--- api.py
+++ api.py
@@ -38,12 +38,11 @@ if __name__ == '__main__':
     parser.add_argument('--debug', action='store_true', help='是否开启调试模式。')
     parser.add_argument('--host', default='0.0.0.0', help='服务监听的 IP 地址。')
     parser.add_argument('--port', default="50050", help='服务监听的端口号。')
-    parser.add_argument('--device', default='cuda', help='使用的设备 (cpu 或 cuda)。')
     parser.add_argument('--model', default='small', help='使用的模型 (small 或 large)。')
     parser.add_argument('--model_dir', default='/workspace/models/DataoceanAI/dolphin-small', help='模型的路径。')
     args = parser.parse_args()
 
     # 加载 Dolphin 模型
-    model = dolphin.load_model(args.model, args.model_dir, device=args.device)
+    model = dolphin.load_model(args.model, args.model_dir)
 
     app.run(debug=args.debug, host=args.host, port=int(args.port))
